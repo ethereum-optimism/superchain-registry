ed62f1d0988a75efa6573761373219c93a068454
George C. Knee <georgeknee@googlemail.com>
2024-01-29 17:42:31 +0000

    Add a `validate` package including new tests for validating contract versions. (#62)
    * chore: update public_rpc for goerli superchains
    
    existing endpoint doesn't work:
    
    curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":1}' https://ethereum-goerli-rpc.allthatnode.com
    {"error": "request has an invalid host info","id":1}
    
    * chore: go mod tidy in /superchain
    
    * fixmeup update endpoints again
    
    * feat: add validation module
    
    and validation package with TestContractVersions
    
    * ci: ensure new module has tests run in CI
    
    * test: add some logging
    
    * docs: be more accurate in godoc
    
    * chore: add go.work test
    
    This enables intellisense in my editor, which otherwise doesn't seem to work when working in the root of the repo
    
    * chore: remove go.work.sum
    
    I'm not sure what this is for
    
    * chore: update go.work
    
    * fix: use strict equality in semver check
    
    * fix: rework to use new per-superchain semver.yaml files
    
    * fix: do not use semver package to compare versions
    
    this silently reports equality if both versions are not formatted correctly (prefixed with a v)
    
    * implement exclusion list
    
    right now this includes all of the chains(!) because none specify SystemConfigProxy versions
    
    * refactor: improve error handling for missing endpoints
    
    * fix linter hint
    
    * improve error handling
    
    * use dot import and add some whitespace
    
    * refactor: use op-service/retry library
    
    * ci: extract linting into a single config and job
    
    * ci: run tests on both modules within golang-test job
    
    * refactor: use op-bindings library to getVersion
    
    * test: use testify/require
    
    * fix: Add SystemConfig entry to requested l1 contract addresses
    
    * fix: missing comma
    
    * fix: Add SystemConfigProxy to AddressList struct
    
    * refactor: use hardcoded lookup functions instead or reflection
    
    * fix: reorder list and add Proxy suffix
    
    * fix: don't forget the other suffix :-P
    
    * refactor: versionFor returns an Address
    
    * test: cover AddressFor and VersionFor methods
    
    * thread context in getVersionWithRetries
    
    * test: unskip {goerli/mainnet/sepolia).OP- chains

---
+++ validation/superchain-version_test.go
@@ -0,0 +1,119 @@
+package validation
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	. "github.com/ethereum-optimism/superchain-registry/superchain"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ethereum-optimism/optimism/op-service/retry"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+// TestContractVersions will check that
+//   - for each chain in OPChain
+//   - for each declared contract "Foo" : version entry in the corresponding superchain's semver.yaml
+//   - the chain has a contract FooProxy deployed at the same version
+//
+// Actual semvers are
+// read from the L1 chain RPC provider for the chain in question.
+func TestContractVersions(t *testing.T) {
+
+	isExcluded := map[uint64]bool{
+		291:          true,
+		424:          true,
+		888:          true,
+		957:          true,
+		997:          true,
+		8453:         true,
+		34443:        true,
+		58008:        true,
+		84531:        true,
+		84532:        true,
+		7777777:      true,
+		11763071:     true,
+		999999999:    true,
+		129831238013: true,
+	}
+
+	isSemverAcceptable := func(desired, actual string) bool {
+		return desired == actual
+	}
+
+	checkOPChainSatisfiesSemver := func(chain *ChainConfig) {
+		rpcEndpoint := Superchains[chain.Superchain].Config.L1.PublicRPC
+
+		require.NotEmpty(t, rpcEndpoint)
+
+		client, err := ethclient.Dial(rpcEndpoint)
+		require.NoErrorf(t, err, "could not dial rpc endpoint %s", rpcEndpoint)
+
+		contractNames := []string{
+			"L1CrossDomainMessenger",
+			"L1ERC721Bridge",
+			"L1StandardBridge",
+			"L2OutputOrcale",
+			"OptimismMintableERC20Factory",
+			"OptimismPortal",
+			"SystemConfig",
+		}
+
+		for _, contractName := range contractNames {
+			desiredSemver, err := SuperchainSemver[chain.Superchain].VersionFor(contractName)
+			require.NoError(t, err)
+
+			// ASSUMPTION: we will check the version of the implementation via the declared proxy contract
+			proxyContractName := contractName + "Proxy"
+
+			contractAddress, err := Addresses[chain.ChainID].AddressFor(proxyContractName)
+			require.NoErrorf(t, err, "%s/%s.%s.version= UNSPECIFIED (desired version %s)", chain.Superchain, chain.Name, proxyContractName, desiredSemver)
+
+			actualSemver, err := getVersionWithRetries(context.Background(), common.Address(contractAddress), client)
+			require.NoErrorf(t, err, "RPC endpoint %s: %s", rpcEndpoint)
+
+			require.Condition(t, func() bool { return isSemverAcceptable(desiredSemver, actualSemver) },
+				"%s/%s.%s.version=%s (UNACCEPTABLE desired version %s)", chain.Superchain, chain.Name, proxyContractName, actualSemver, desiredSemver)
+
+			t.Logf("%s/%s.%s.version=%s (acceptable compared to %s)", chain.Superchain, chain.Name, proxyContractName, actualSemver, desiredSemver)
+
+		}
+	}
+
+	for chainID, chain := range OPChains {
+		if !isExcluded[chainID] {
+			checkOPChainSatisfiesSemver(chain)
+		}
+	}
+}
+
+// getVersion will get the version of a contract at a given address, if it exposes a version() method.
+func getVersion(ctx context.Context, addr common.Address, client *ethclient.Client) (string, error) {
+	isemver, err := bindings.NewISemver(addr, client)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", addr, err)
+	}
+	version, err := isemver.Version(&bind.CallOpts{
+		Context: ctx,
+	})
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", addr, err)
+	}
+
+	return version, nil
+
+}
+
+// getVersionWithRetries is a wrapper for getVersion
+// which retries up to 10 times with exponential backoff.
+func getVersionWithRetries(ctx context.Context, addr common.Address, client *ethclient.Client) (string, error) {
+	const maxAttempts = 10
+	return retry.Do(ctx, maxAttempts, retry.Exponential(), func() (string, error) {
+		return getVersion(context.Background(), addr, client)
+	})
+}
