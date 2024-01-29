62626039205aebd7d54c7db1048d41f96a53a235
geoknee <georgeknee@googlemail.com>
2024-01-29 14:22:07 +0000

    test: cover AddressFor and VersionFor methods

--- superchain/superchain_test.go
+++ superchain/superchain_test.go
@@ -9,6 +9,35 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+func TestAddressFor(t *testing.T) {
+	al := AddressList{
+		ProxyAdmin:     HexToAddress("0xD98bD7a1F2384D890d0D6153CbCFcCF6F813ab6c"),
+		AddressManager: Address{},
+	}
+	want := HexToAddress("0xD98bD7a1F2384D890d0D6153CbCFcCF6F813ab6c")
+	got, err := al.AddressFor("ProxyAdmin")
+	require.NoError(t, err)
+	require.Equal(t, want, got)
+	_, err = al.AddressFor("AddressManager")
+	require.Error(t, err)
+	_, err = al.AddressFor("Garbage")
+	require.Error(t, err)
+}
+
+func TestVersionFor(t *testing.T) {
+	cl := ContractVersions{
+		L1CrossDomainMessenger: "1.9.9",
+		OptimismPortal:         "",
+	}
+	want := "1.9.9"
+	got, err := cl.VersionFor("L1CrossDomainMessenger")
+	require.NoError(t, err)
+	require.Equal(t, want, got)
+	_, err = cl.VersionFor("OptimismPortal")
+	require.Error(t, err)
+	_, err = cl.VersionFor("Garbage")
+	require.Error(t, err)
+}
 func TestChainIds(t *testing.T) {
 	chainIDs := map[uint64]bool{}
 
