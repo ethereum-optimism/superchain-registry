38af98744883297c79776e15f517ed7489acb8fe
geoknee <georgeknee@googlemail.com>
2024-02-05 09:22:13 +0000

    feat: expose OPMainnetResourceConfig
    this is currently used as the "ground truth" for all OP chains, regardless of superchain target.

--- validation/resource-config_test.go
+++ validation/resource-config_test.go
@@ -3,7 +3,6 @@ package validation
 import (
 	"context"
 	"fmt"
-	"math/big"
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
@@ -37,23 +36,10 @@ func TestResourceConfig(t *testing.T) {
 		contractAddress, err := Addresses[chain.ChainID].AddressFor("SystemConfigProxy")
 		require.NoError(t, err)
 
-		uint128Max, ok := new(big.Int).SetString("ffffffffffffffffffffffffffffffff", 16)
-		if !ok {
-			panic("cannot construct uint128Max")
-		}
-		desiredResourceConfig := bindings.ResourceMeteringResourceConfig{
-			MaxResourceLimit:            20000000,
-			ElasticityMultiplier:        10,
-			BaseFeeMaxChangeDenominator: 8,
-			MinimumBaseFee:              1000000000,
-			SystemTxMaxGas:              1000000,
-			MaximumBaseFee:              uint128Max,
-		} // from OP Mainnet
-
 		actualResourceConfig, err := getResourceConfigWithRetries(context.Background(), common.Address(contractAddress), client)
 		require.NoErrorf(t, err, "RPC endpoint %s: %s", rpcEndpoint)
 
-		require.Equal(t, desiredResourceConfig, actualResourceConfig, "resource config unacceptable")
+		require.Equal(t, bindings.ResourceMeteringResourceConfig(OPMainnetResourceConfig), actualResourceConfig, "resource config unacceptable")
 
 		t.Logf("resource metering acceptable")
 
