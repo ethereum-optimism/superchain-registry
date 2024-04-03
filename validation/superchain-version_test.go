f945ddb7eeb0ddf13b9ce739e4a6d0b35232f2dd
George C. Knee <georgeknee@googlemail.com>
2024-04-03 20:04:52 +0100

    Temporarily exclude standard conduit chains from contract version checks (#159)
    * excludle mainnet/pontem from contract version checks
    
    https://github.com/ethereum-optimism/security-pod/issues/105
    
    * exclude sepolia/mode from contract version checks
    
    https://github.com/ethereum-optimism/security-pod/issues/105
    
    * exclude mainnet/metal from contract version checking
    
    https://github.com/ethereum-optimism/security-pod/issues/105
    
    * exclude sepolia/metal from contract version checking
    
    https://github.com/ethereum-optimism/security-pod/issues/105

--- validation/superchain-version_test.go
+++ validation/superchain-version_test.go
@@ -53,7 +53,11 @@ func TestSuperchainWideContractVersions(t *testing.T) {
 
 func TestContractVersions(t *testing.T) {
 	isExcluded := map[uint64]bool{
+		919:       true, // sepolia/mode   L1CrossDomainMessengerProxy.version=1.4.1, https://github.com/ethereum-optimism/security-pod/issues/105
+		1740:      true, // sepolia/metal  L1CrossDomainMessengerProxy.version=1.4.1, https://github.com/ethereum-optimism/security-pod/issues/105
+		1750:      true, // mainnet/metal  L1CrossDomainMessengerProxy.version=1.4.1, https://github.com/ethereum-optimism/security-pod/issues/105
 		8453:      true, // mainnet/base
+		8866:      true, // mainnet/pontem L1CrossDomainMessengerProxy.version=1.4.1, https://github.com/ethereum-optimism/security-pod/issues/105
 		34443:     true, // mainnet/mode
 		84532:     true, // sepolia/base
 		90001:     true, // sepolia/race, due to https://github.com/ethereum-optimism/superchain-registry/issues/147
