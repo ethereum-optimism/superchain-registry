c7a85e2b11d19dce9435f3aeda25b7505fd741e4
Tess Rinearson <tess.rinearson@gmail.com>
2024-04-16 23:32:11 +0200

    update with George feedback

--- validation/doc.go
+++ validation/doc.go
@@ -1,20 +1,17 @@
 // Package validation contains logic to confirm that an OP Chain is either a valid Standard Chain
 // or a valid Frontier Chain. These checks are provided as Go tests.
 
-// It performs the following checks:
-
-// For Standard Chains:
-
+// It performs the following checks, for all chains:
 // * The Chain ID is unique.
 // * There is a valid, public RPC endpoint which returns a valid response.
+//   (This is checked implicitly; other checks will fail if we don't have a valid RPC endpoint.)
+//
+// For Standard Chains, we perform the following additional checks:
+//
 // * The resource metering config matches the Standard config.
 //   This includes the resource limit, elasticity multiplier, base fee maximum change denominator, minimum base fee, maximum base fee, and maximum gas.
 // * There is a valid gas price oracle, and the parameters (blob base fee scalar and base feed scalar) are within the permitted range.
-// * The L2 output oracle parameters (submission interval, block time and finalization period) all match the Standard config.
+// * The L2 output oracle parameters (submission interval, block time and finalization period) all match the values for OP Mainnet.
 // * The calculated genesis hash matches the hash declared in the chain’s config file.
-
-// For Frontier Chains:
-// * The Chain ID is unique.
-// * There is a valid, public RPC endpoint which returns a valid response.
 
 package validation
