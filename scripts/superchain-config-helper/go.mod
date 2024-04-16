5c1696a6109800d430fe7d3a1f6cf29c7fb03da0
Blaine <blainemalone01@gmail.com>
2024-04-16 16:07:25 -0400

    fix: cmd line

--- scripts/superchain-config-helper/go.mod
+++ scripts/superchain-config-helper/go.mod
@@ -4,3 +4,11 @@ go 1.22.0
 
 require gopkg.in/yaml.v2 v2.4.0
 
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- scripts/superchain-config-helper/superchain.go
+++ scripts/superchain-config-helper/superchain.go
@@ -3,105 +3,48 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
-	"log"
 	"math/big"
 	"net/http"
 	"os"
 	"strings"
 	"text/tabwriter"
 
+	oplog "github.com/ethereum-optimism/optimism/op-service/log"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/mattn/go-isatty"
+	"github.com/urfave/cli/v2"
+
 	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	rpcURL := flag.String("rpcURL", "https://eth-mainnet.g.alchemy.com/v2/6eCsAcYZmnnoS-FV6PCfqmObgK0prq-u", "RPC URL")
-	network := flag.String("network", "mainnet", "Network (mainnet or sepolia)")
-
-	flag.Parse()
-
-	if flag.Arg(0) == "help" {
-		flag.Usage()
-		return
+	color := isatty.IsTerminal(os.Stderr.Fd())
+	oplog.SetGlobalLogHandler(log.NewTerminalHandler(os.Stderr, color))
+	app := &cli.App{
+		Name:  "superchain",
+		Usage: "Summarize the configuration of each chain in the Superchain.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "l1-rpc-url",
+				Value:   "https://eth-mainnet.g.alchemy.com/v2/6eCsAcYZmnnoS-FV6PCfqmObgK0prq-u",
+				Usage:   "L1 RPC URL",
+				EnvVars: []string{"L1_RPC_URL"},
+			},
+			&cli.StringFlag{
+				Name:    "network",
+				Value:   "mainnet",
+				Usage:   "L1 RPC URL (mainnet or sepolia)",
+				EnvVars: []string{"SUPERCHAIN_TARGET"},
+			},
+		},
+		Action: entrypoint,
 	}
 
-	if *network == "" {
-		fmt.Println("Error: network is required")
-		flag.Usage()
-		os.Exit(1)
+	if err := app.Run(os.Args); err != nil {
+		log.Crit("error op-upgrade", "err", err)
 	}
-	fmt.Println("Network:", *network)
-
-	if *rpcURL == "" {
-		fmt.Println("Error: rpcURL is required")
-		flag.Usage()
-		os.Exit(1)
-	}
-	fmt.Println("RPC URL:", *rpcURL)
-
-	chains := []string{"base", "mode", "op", "zora"}
-	genesisPath := "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/superchain/configs"
-	l1AddressesPath := "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/superchain/extra/addresses"
-
-	tw := tabwriter.NewWriter(os.Stdout, 20, 0, 2, ' ', 0)
-	caption := "\033[1mSuperchain Configuration\033[0m"
-	rows := [][]string{}
-	chainIdRow := []string{"\033[1mChain ID\033[0m"}
-	batchInboxAddressRow := []string{"\033[1mBatch Inbox Address\033[0m"}
-	explorerUrlRow := []string{"\033[1mExplorer URL\033[0m"}
-	challengePeriodRow := []string{"\033[1mChallenge Period\033[0m"}
-	feeScalarRow := []string{"\033[1mFee Scalar\033[0m"}
-	gasLimitRow := []string{"\033[1mGas Limit\033[0m"}
-	genesisStateRow := []string{"\033[1mGenesis State\033[0m"}
-	l2BlockTimeRow := []string{"\033[1mL2 Block Time\033[0m"}
-
-	for _, chain := range chains {
-		fmt.Printf("\nFetching for %s...\n", chain)
-		genesisUrl := fmt.Sprintf("%s/%s/%s.yaml", genesisPath, *network, chain)
-		var genesisConfig GenesisConfig
-		genesisConfig.loadGenesisConfig(genesisUrl)
-
-		l1AddressesUrl := fmt.Sprintf("%s/%s/%s.json", l1AddressesPath, *network, chain)
-		var l1AddressesConfig L1AddressesConfig
-		l1AddressesConfig.loadL1AddressesConfig(l1AddressesUrl)
-
-		chainIdRow = append(chainIdRow, fmt.Sprint(genesisConfig.ChainID))
-		batchInboxAddressRow = append(batchInboxAddressRow, genesisConfig.BatchInboxAddr)
-		explorerUrlRow = append(explorerUrlRow, fmt.Sprintf("\x1b]8;;%s\x1b\\%s\x1b]8;;\x1b\\", genesisConfig.Explorer, genesisConfig.Explorer))
-
-		// challengePeriod, _ := getChallengePeriod(*rpcURL, l1AddressesConfig.L2OutputOracleProxy)
-
-		challengePeriod, _ := getEitherOnChainInteger(*rpcURL, l1AddressesConfig.L2OutputOracleProxy, "0xce5db8d6", "0xf4daa291")
-		challengePeriodRow = append(challengePeriodRow, challengePeriod)
-
-		feeScalar, _ := getFeeScalar(*rpcURL, l1AddressesConfig.SystemConfigProxy)
-		feeScalarRow = append(feeScalarRow, "Fixed: "+feeScalar.FixedData+" Dynamic: "+feeScalar.DynamicData)
-
-		gasLimitRow = append(gasLimitRow, "TODO")
-
-		genesisStateRow = append(genesisStateRow, genesisConfig.Genesis.L2.Hash)
-
-		l2BlockTime, _ := getEitherOnChainInteger(*rpcURL, l1AddressesConfig.L2OutputOracleProxy, "0x93991af3", "0x002134cc")
-		l2BlockTimeRow = append(l2BlockTimeRow, l2BlockTime+" seconds")
-	}
-	rows = append(rows, chainIdRow)
-	rows = append(rows, batchInboxAddressRow)
-	rows = append(rows, explorerUrlRow)
-	rows = append(rows, challengePeriodRow)
-	rows = append(rows, feeScalarRow)
-	rows = append(rows, gasLimitRow)
-	rows = append(rows, genesisStateRow)
-	rows = append(rows, l2BlockTimeRow)
-	if caption != "" {
-		fmt.Fprintf(tw, "\n\n%s:\n\n", caption)
-	}
-	fmt.Fprintln(tw, strings.Join(append([]string{"\033[1mParameter\033[0m"}, chains...), "\t"))
-	for _, row := range rows {
-		fmt.Fprintln(tw, strings.Join(row, "\t"))
-	}
-	tw.Flush()
 
 	// address := "0x229047fed2591dbec1eF1118d64F7aF3dB9EB290"
 	// functionSignature := "resourceConfig()((uint32,uint8,uint8,uint32,uint32,uint128))"
@@ -132,19 +75,71 @@ func main() {
 	// fmt.Printf("Result: %+v\n", result)
 }
 
-// func getL2BlockTime(rpcURL string, l2OutputOracleProxy string) (string, error) {
-// 	var l2BlockTime string
-// 	var err error
+func entrypoint(ctx *cli.Context) error {
+	rpcURL := ctx.String("l1-rpc-url")
+	network := ctx.String("network")
 
-// 	l2BlockTime, err = getOnChainInteger(rpcURL, l2OutputOracleProxy, "0x93991af3") // l2BlockTime()
-// 	if l2BlockTime == "0" || err != nil {
-// 		l2BlockTime, err = getOnChainInteger(rpcURL, l2OutputOracleProxy, "0x002134cc") // L2_BLOCK_TIME()
-// 		if err != nil {
-// 			return "", fmt.Errorf("there was an error fetching the l2BlockTime: %w", err)
-// 		}
-// 	}
-// 	return l2BlockTime, nil
-// }
+	chains := []string{"base", "mode", "op", "zora"}
+	genesisPath := "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/superchain/configs"             // Can't I just read locally?
+	l1AddressesPath := "https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/superchain/extra/addresses" // Can't I just read locally?
+
+	tw := tabwriter.NewWriter(os.Stdout, 20, 0, 2, ' ', 0)
+	caption := "\033[1mSuperchain Configuration\033[0m"
+	rows := [][]string{}
+	chainIdRow := []string{"\033[1mChain ID\033[0m"}
+	batchInboxAddressRow := []string{"\033[1mBatch Inbox Address\033[0m"}
+	explorerUrlRow := []string{"\033[1mExplorer URL\033[0m"}
+	challengePeriodRow := []string{"\033[1mChallenge Period\033[0m"}
+	feeScalarRow := []string{"\033[1mFee Scalar\033[0m"}
+	gasLimitRow := []string{"\033[1mGas Limit\033[0m"}
+	genesisStateRow := []string{"\033[1mGenesis State\033[0m"}
+	l2BlockTimeRow := []string{"\033[1mL2 Block Time\033[0m"}
+
+	for _, chain := range chains {
+		fmt.Printf("\nFetching for %s...\n", chain)
+		genesisUrl := fmt.Sprintf("%s/%s/%s.yaml", genesisPath, network, chain)
+		var genesisConfig GenesisConfig
+		genesisConfig.loadGenesisConfig(genesisUrl)
+
+		l1AddressesUrl := fmt.Sprintf("%s/%s/%s.json", l1AddressesPath, network, chain)
+		var l1AddressesConfig L1AddressesConfig
+		l1AddressesConfig.loadL1AddressesConfig(l1AddressesUrl)
+
+		chainIdRow = append(chainIdRow, fmt.Sprint(genesisConfig.ChainID))
+		batchInboxAddressRow = append(batchInboxAddressRow, genesisConfig.BatchInboxAddr)
+		explorerUrlRow = append(explorerUrlRow, fmt.Sprintf("\x1b]8;;%s\x1b\\%s\x1b]8;;\x1b\\", genesisConfig.Explorer, genesisConfig.Explorer))
+
+		challengePeriod, _ := getEitherOnChainInteger(rpcURL, l1AddressesConfig.L2OutputOracleProxy, "0xce5db8d6", "0xf4daa291")
+		challengePeriodRow = append(challengePeriodRow, challengePeriod)
+
+		feeScalar, _ := getFeeScalar(rpcURL, l1AddressesConfig.SystemConfigProxy)
+		feeScalarRow = append(feeScalarRow, "Fixed: "+feeScalar.FixedData+" Dynamic: "+feeScalar.DynamicData)
+
+		gasLimitRow = append(gasLimitRow, "TODO")
+
+		genesisStateRow = append(genesisStateRow, genesisConfig.Genesis.L2.Hash)
+
+		l2BlockTime, _ := getEitherOnChainInteger(rpcURL, l1AddressesConfig.L2OutputOracleProxy, "0x93991af3", "0x002134cc")
+		l2BlockTimeRow = append(l2BlockTimeRow, l2BlockTime+" seconds")
+	}
+	rows = append(rows, chainIdRow)
+	rows = append(rows, batchInboxAddressRow)
+	rows = append(rows, explorerUrlRow)
+	rows = append(rows, challengePeriodRow)
+	rows = append(rows, feeScalarRow)
+	rows = append(rows, gasLimitRow)
+	rows = append(rows, genesisStateRow)
+	rows = append(rows, l2BlockTimeRow)
+	if caption != "" {
+		fmt.Fprintf(tw, "\n\n%s:\n\n", caption)
+	}
+	fmt.Fprintln(tw, strings.Join(append([]string{"\033[1mParameter\033[0m"}, chains...), "\t"))
+	for _, row := range rows {
+		fmt.Fprintln(tw, strings.Join(row, "\t"))
+	}
+	tw.Flush()
+	return nil
+}
 
 func getFeeScalar(rpcURL string, systemConfigProxy string) (FeeScalar, error) {
 	fixedData, _ := getOnChainInteger(rpcURL, systemConfigProxy, "0x0c18c162") // overhead()
@@ -257,18 +252,18 @@ type L1AddressesConfig struct {
 func loadConfig(url string, unmarshalFunc func([]byte, interface{}) error, config interface{}) interface{} {
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("HTTP GET request failed: %v", err)
+		//log.Fatalf("HTTP GET request failed: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("Reading response body failed: %v", err)
+		//log.Fatalf("Reading response body failed: %v", err)
 	}
 
 	err = unmarshalFunc(body, config)
 	if err != nil {
-		log.Fatalf("Error parsing file (skipping): %s", url)
+		//log.Fatalf("Error parsing file (skipping): %s", url)
 	}
 	return config
 }
