0496fcd6ee4e36f2bedd3e4fbffa9146b10219af
George C. Knee <georgeknee@googlemail.com>
2024-04-17 14:18:38 +0100

    Add  `standard-config.toml` and use in validation tests (#185)
    * sketch out pattern to declare standard config in toml
    
    * tidy up
    
    * go mod tidy
    
    * remove unused fn
    
    * remove comment in toml file
    
    * remove blank line

--- validation/resource-config_test.go
+++ validation/resource-config_test.go
@@ -36,7 +36,7 @@ func TestResourceConfig(t *testing.T) {
 		actualResourceConfig, err := getResourceConfigWithRetries(context.Background(), common.Address(contractAddress), client)
 		require.NoErrorf(t, err, "RPC endpoint %s: %s", rpcEndpoint)
 
-		require.Equal(t, bindings.ResourceMeteringResourceConfig(OPMainnetResourceConfig), actualResourceConfig, "resource config unacceptable")
+		require.Equal(t, bindings.ResourceMeteringResourceConfig(StandardConfig.ResourceConfig), actualResourceConfig, "resource config unacceptable")
 	}
 
 	for _, chain := range OPChains {
