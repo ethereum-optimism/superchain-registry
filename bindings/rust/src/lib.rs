d969223f8f495260e77637887b97e3fe06f86058
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-06-05 23:26:00 +0200

    feat: added chain config deserialization tests

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -40,7 +40,9 @@ lazy_static::lazy_static! {
 
 #[cfg(test)]
 mod tests {
-    use crate::SUPERCHAINS;
+    use alloy_primitives::{address, b256};
+
+    use crate::{superchain::SuperchainLevel, OPCHAINS, SUPERCHAINS};
 
     #[test]
     fn test_read_superchains() {
@@ -56,5 +58,42 @@ mod tests {
         let mainnet = superchains.get("mainnet").unwrap();
         assert_eq!(mainnet.config.name, "Mainnet");
         assert_eq!(mainnet.chain_ids.len(), 8);
+
+        let base_chain_id = 8453;
+        let opchains = OPCHAINS.clone();
+        let base = opchains.get(&base_chain_id).unwrap();
+        assert_eq!(base.chain_id, base_chain_id);
+        assert_eq!(base.name, "Base");
+        assert_eq!(base.superchain, "mainnet");
+        assert_eq!(base.chain, "base");
+        assert_eq!(base.public_rpc, "https://mainnet.base.org");
+        assert_eq!(base.sequencer_rpc, "https://mainnet-sequencer.base.org");
+        assert_eq!(base.explorer, "https://explorer.base.org");
+
+        assert!(matches!(base.superchain_level, SuperchainLevel::Standard));
+        assert_eq!(base.superchain_time, Some(0));
+        assert_eq!(
+            base.batch_inbox_addr,
+            address!("ff00000000000000000000000000000000008453")
+        );
+
+        assert_eq!(base.genesis.l1.number, 17481768);
+        assert_eq!(
+            base.genesis.l1.hash,
+            b256!("5c13d307623a926cd31415036c8b7fa14572f9dac64528e857a470511fc30771")
+        );
+        assert_eq!(base.genesis.l2.number, 0);
+        assert_eq!(
+            base.genesis.l2.hash,
+            b256!("f712aa9241cc24369b143cf6dce85f0902a9731e70d66818a3a5845b296c73dd")
+        );
+        assert_eq!(base.genesis.l2_time, 1686789347);
+        assert_eq!(base.genesis.extra_data, None);
+        assert!(base.genesis.system_config.is_none());
+
+        assert_eq!(base.hardfork_configuration.canyon_time, None);
+        assert_eq!(base.hardfork_configuration.delta_time, None);
+        assert_eq!(base.hardfork_configuration.ecotone_time, None);
+        assert_eq!(base.hardfork_configuration.fjord_time, None);
     }
 }
