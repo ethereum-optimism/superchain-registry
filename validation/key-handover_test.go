c1f34b5b6ad9ef29158bbf75f23be4a5c2f52a3c
geoknee <georgeknee@googlemail.com>
2024-06-21 11:11:52 +0100

    split out and document key handover addresses
    reinstate op chains on sepolia and sepolia-dev-0 to standard level

--- validation/key-handover_test.go
+++ validation/key-handover_test.go
@@ -4,14 +4,21 @@ import (
 	"testing"
 
 	. "github.com/ethereum-optimism/superchain-registry/superchain"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+var Multisig = map[string]Address{
+	// On each superchain, this is a 2/2 Safe between the Optimism Foundation and the Security Council
+	"mainnet":       MustHexToAddress("0x5a0Aae59D09fccBdDb6C6CcEB07B7279367C3d2A"),
+	"sepolia":       MustHexToAddress("0x1Eb2fFc903729a0F03966B917003800b145F56E2"),
+	"sepolia-dev-0": MustHexToAddress("0x4377BB0F0103992b31eC12b4d796a8687B8dC8E9"),
+}
+
 func testKeyHandoverOfChain(t *testing.T, chainID uint64) {
-	rpcEndpoint := Superchains[OPChains[chainID].Superchain].Config.L1.PublicRPC
+	superchain := OPChains[chainID].Superchain
+	rpcEndpoint := Superchains[superchain].Config.L1.PublicRPC
 	require.NotEmpty(t, rpcEndpoint, "no rpc specified")
 
 	client, err := ethclient.Dial(rpcEndpoint)
@@ -20,10 +27,9 @@ func testKeyHandoverOfChain(t *testing.T, chainID uint64) {
 	proxyAdmin, err := Addresses[chainID].AddressFor("ProxyAdmin")
 	require.NoError(t, err)
 
+	want := Multisig[superchain]
 	got, err := getAddress("owner()", proxyAdmin, client)
 	require.NoError(t, err)
-
-	want := Address(common.HexToAddress("0x5a0Aae59D09fccBdDb6C6CcEB07B7279367C3d2A"))
 
 	assert.Equal(t, want, got, "%s.%s = %s, expected %s (%s)", proxyAdmin, "owner()", got, want, "ProxyAdminOwner")
 }
