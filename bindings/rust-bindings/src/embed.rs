880dc561833ae196dc11a93595563638e3ddb30b
refcell <abigger87@gmail.com>
2024-06-21 14:18:31 -0400

    fix: embed path

--- bindings/rust-bindings/src/embed.rs
+++ bindings/rust-bindings/src/embed.rs
@@ -2,11 +2,11 @@ use include_dir::{include_dir, Dir};
 
 /// Directory containing the configuration files for the superchain.
 pub(crate) static CONFIGS_DIR: Dir<'_> =
-    include_dir!("$CARGO_MANIFEST_DIR/../../../../superchain/configs");
+    include_dir!("$CARGO_MANIFEST_DIR/../../../../../superchain/configs");
 
 /// Directory containing the extra files for the superchain.
-pub(crate) static EXTRA_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../../../../superchain/extra");
+pub(crate) static EXTRA_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../../../../../superchain/extra");
 
 /// Directory containing the implementation addresses for the superchain.
 pub(crate) static IMPLEMENTATIONS_DIR: Dir<'_> =
-    include_dir!("$CARGO_MANIFEST_DIR/../../../../superchain/implementations");
+    include_dir!("$CARGO_MANIFEST_DIR/../../../../../superchain/implementations");
