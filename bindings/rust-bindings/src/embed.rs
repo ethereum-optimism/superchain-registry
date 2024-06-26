949179a508af955d89775c3ef5cc7cd1eddfbf6a
clabby <ben@clab.by>
2024-06-25 20:38:33 -0400

    fix: release plz embeddings

--- bindings/rust-bindings/src/embed.rs
+++ bindings/rust-bindings/src/embed.rs
@@ -1,12 +1,11 @@
 use include_dir::{include_dir, Dir};
 
 /// Directory containing the configuration files for the superchain.
-pub(crate) static CONFIGS_DIR: Dir<'_> =
-    include_dir!("$CARGO_MANIFEST_DIR/../../superchain/configs");
+pub(crate) static CONFIGS_DIR: Dir<'_> = include_dir!("$SUPERCHAIN_CONFIGS");
 
 /// Directory containing the extra files for the superchain.
-pub(crate) static EXTRA_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../../superchain/extra");
+pub(crate) static EXTRA_DIR: Dir<'_> = include_dir!("$SUPERCHAIN_EXTRA");
 
 /// Directory containing the implementation addresses for the superchain.
-pub(crate) static IMPLEMENTATIONS_DIR: Dir<'_> =
-    include_dir!("$CARGO_MANIFEST_DIR/../../superchain/implementations");
+pub(crate) static IMPLEMENTATIONS_DIR: Dir<'_> = include_dir!("$SUPERCHAIN_IMPLEMENTATIONS");
+
