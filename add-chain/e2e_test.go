9ce1a03ea51fe7fae833bfbc3157acc4e1554ef4
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-06-25 22:33:38 -0400

    Run check-rollup-config test after main add-chain test completes

--- add-chain/e2e_test.go
+++ add-chain/e2e_test.go
@@ -11,49 +11,54 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func TestCLIApp(t *testing.T) {
-	tests := []struct {
-		name                   string
-		chainName              string
-		rollupConfigFile       string
-		standardChainCandidate bool
-		chainType              string
-		deploymentsDir         string
-	}{
-		{
-			name:             "baseline",
-			chainName:        "awesomechain_baseline",
-			rollupConfigFile: "./testdata/monorepo/op-node/rollup_baseline.json",
-			chainType:        "standard",
-		},
-		{
-			name:             "plasma",
-			chainName:        "awesomechain_plasma",
-			rollupConfigFile: "./testdata/monorepo/op-node/rollup_plasma.json",
-			chainType:        "standard",
-		},
-		{
-			name:                   "standard-candidate",
-			chainName:              "awesomechain_standard-candidate",
-			rollupConfigFile:       "./testdata/monorepo/op-node/rollup_baseline.json",
-			chainType:              "frontier",
-			standardChainCandidate: true,
-		},
-		{
-			name:             "faultproofs",
-			chainName:        "awesomechain_faultproofs",
-			rollupConfigFile: "./testdata/monorepo/op-node/rollup_faultproofs.json",
-			chainType:        "standard",
-			deploymentsDir:   "./testdata/monorepo/deployments-faultproofs",
-		},
-	}
+var tests = []struct {
+	name                   string
+	chainName              string
+	rollupConfigFile       string
+	standardChainCandidate bool
+	chainType              string
+	chainId                uint64
+	deploymentsDir         string
+}{
+	{
+		name:             "baseline",
+		chainName:        "testchain_baseline",
+		rollupConfigFile: "./testdata/monorepo/op-node/rollup_baseline.json",
+		chainType:        "standard",
+		chainId:          4206900,
+	},
+	{
+		name:             "plasma",
+		chainName:        "testchain_plasma",
+		rollupConfigFile: "./testdata/monorepo/op-node/rollup_plasma.json",
+		chainType:        "standard",
+		chainId:          4206901,
+	},
+	{
+		name:                   "standard-candidate",
+		chainName:              "testchain_standard-candidate",
+		rollupConfigFile:       "./testdata/monorepo/op-node/rollup_standard-candidate.json",
+		chainType:              "frontier",
+		chainId:                4206902,
+		standardChainCandidate: true,
+	},
+	{
+		name:             "faultproofs",
+		chainName:        "testchain_faultproofs",
+		rollupConfigFile: "./testdata/monorepo/op-node/rollup_faultproofs.json",
+		chainType:        "standard",
+		chainId:          4206903,
+		deploymentsDir:   "./testdata/monorepo/deployments-faultproofs",
+	},
+}
 
+func TestAddChain_Main(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			defer cleanupTestFiles(t, tt.chainName)
+			cleanupTestFiles(t, tt.chainName)
 
 			args := []string{
 				"add-chain",
@@ -74,17 +79,25 @@ func TestCLIApp(t *testing.T) {
 			checkConfigYaml(t, tt.name, tt.chainName)
 			compareJsonFiles(t, "superchain/extra/addresses/sepolia/", tt.name, tt.chainName)
 			compareJsonFiles(t, "superchain/extra/genesis-system-configs/sepolia/", tt.name, tt.chainName)
+		})
+	}
+}
 
+func TestAddChain_CheckRollupConfig(t *testing.T) {
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			// Must run the following subcommand after the main command completes so that when the op-node/rollup
 			// package imports the superchain package, the superchain package includes the output test files
-			//args = []string{
-			//"add-chain",
-			//"check-rollup-config",
-			//"--chain-id=" + "42069",
-			//"--rollup-config=" + tt.rollupConfigFile,
-			//}
-			//err = app.Run(args)
-			//require.NoError(t, err, "add-chain app failed")
+			args := []string{
+				"add-chain",
+				"check-rollup-config",
+				"--chain-id=" + strconv.FormatUint(tt.chainId, 10),
+				"--rollup-config=" + tt.rollupConfigFile,
+			}
+			err := app.Run(args)
+			require.NoError(t, err, "add-chain check-rollup-config failed")
 		})
 	}
 }
@@ -117,7 +130,7 @@ func checkConfigYaml(t *testing.T, testName, chainName string) {
 	require.NoError(t, err, "failed to unmarshal expected.yaml config file: %w", err)
 
 	testBytes, err := os.ReadFile("../superchain/configs/sepolia/" + chainName + ".yaml")
-	require.NoError(t, err, "failed to read awesomechain.yaml config file: %w", err)
+	require.NoError(t, err, "failed to read testchain.yaml config file: %w", err)
 
 	require.Equal(t, string(expectedBytes), string(testBytes), "test .yaml contents do not meet expectation")
 }
