1fd5c20e624fb4d392523e5feedb3e1e46b74272
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-07-11 15:31:28 -0400

    add-chain: main writes toml chain config

--- add-chain/main.go
+++ add-chain/main.go
@@ -135,8 +135,8 @@ func entrypoint(ctx *cli.Context) error {
 		return fmt.Errorf("failed to construct rollup config: %w", err)
 	}
 
-	targetFilePath := filepath.Join(targetDir, chainShortName+".yaml")
-	err = config.WriteChainConfig(rollupConfig, targetFilePath)
+	targetFilePath := filepath.Join(targetDir, chainShortName+".toml")
+	err = config.WriteChainConfigTOML(rollupConfig, targetFilePath)
 	if err != nil {
 		return fmt.Errorf("error generating chain config .yaml file: %w", err)
 	}
