d029e223ced586bed95fa7fd4d0ec2db0c82c6e4
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-07-12 16:19:30 -0400

    add-chain: convert-to-toml writes addresses, genesis.system_config

--- add-chain/cmd/convert-to-toml.go
+++ add-chain/cmd/convert-to-toml.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"path/filepath"
 	"runtime"
 
@@ -30,7 +32,35 @@ var ConvertToTOMLCmd = cli.Command{
 		superchainRepoPath := filepath.Dir(filepath.Dir(filepath.Dir(thisFile)))
 		targetDir := filepath.Join(superchainRepoPath, "superchain", "configs", chain.Superchain)
 		targetFilePath := filepath.Join(targetDir, chain.Chain+".toml")
-		err := config.WriteChainConfigTOML(*chain, targetFilePath)
+
+		var addressList superchain.AddressList
+		addressesPath := filepath.Join(superchainRepoPath, "superchain", "extra", "addresses", chain.Superchain, chain.Chain+".json")
+		rawData, err := os.ReadFile(addressesPath)
+		if err != nil {
+			return fmt.Errorf("failed to read file: %w", err)
+		}
+
+		if err = json.Unmarshal(rawData, &addressList); err != nil {
+			return fmt.Errorf("failed to unmarshal json: %w", err)
+		}
+
+		chain.Addresses = addressList
+
+		var genesisSystemConfig superchain.SystemConfig
+		genesisPath := filepath.Join(superchainRepoPath, "superchain", "extra", "genesis-system-configs", chain.Superchain, chain.Chain+".json")
+		rawData, err = os.ReadFile(genesisPath)
+		if err != nil {
+			return fmt.Errorf("failed to read file: %w", err)
+		}
+
+		if err = json.Unmarshal(rawData, &genesisSystemConfig); err != nil {
+			return fmt.Errorf("failed to unmarshal json: %w", err)
+		}
+
+		chain.Addresses = addressList
+		chain.Genesis.SystemConfig = genesisSystemConfig
+
+		err = config.WriteChainConfigTOML(*chain, targetFilePath)
 		if err != nil {
 			panic(err)
 		}
