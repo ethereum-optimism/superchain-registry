17bf3a8e8c2624054fdd34713ffef2fded042eae
geoknee <georgeknee@googlemail.com>
2024-07-19 10:17:30 +0100

    add remaining gas token checks

--- validation/gas-token_test.go
+++ validation/gas-token_test.go
@@ -2,6 +2,8 @@ package validation
 
 import (
 	"context"
+	"errors"
+	"strings"
 	"testing"
 
 	. "github.com/ethereum-optimism/superchain-registry/superchain"
@@ -14,22 +16,45 @@ import (
 
 func testGasToken(t *testing.T, chain *ChainConfig) {
 	skipIfExcluded(t, chain.ChainID)
-	// Create an ethclient connection to the specified RPC URL
+
 	client, err := ethclient.Dial(chain.PublicRPC)
 	require.NoError(t, err, "Failed to connect to the Ethereum client at RPC url %s", chain.PublicRPC)
 	defer client.Close()
 
+	// WETH predeploy name() check
 	want := "0000000000000000000000000000000000000000000000000000000000000020" + // offset
 		"000000000000000000000000000000000000000000000000000000000000000d" + // length
 		"5772617070656420457468657200000000000000000000000000000000000000" // "Wrapped Ether" padded to 32 bytes
-
-	got, err := getString("name()", Address(common.HexToAddress("0x4200000000000000000000000000000000000006")), client)
+	gotName, err := getString("name()", MustHexToAddress("0x4200000000000000000000000000000000000006"), client)
 	require.NoError(t, err)
+	require.Equal(t, want, gotName)
 
-	require.Equal(t, want, got)
+	// L1Block .isCustomGasToken() check
+	got, err := getBool("isCustomGasToken()", MustHexToAddress("0x4200000000000000000000000000000000000015"), client)
+	if !strings.Contains(err.Error(), "execution reverted") {
+		// Pre: Custom Gas Token feature. Reverting is acceptable.
+		require.NoError(t, err)
+	} else {
+		// Post: Custom Gas Token fearure. Must be set to false.
+		require.False(t, got)
+	}
+
+	// SystemConfig .isCustomGasToken() check (L1)
+	client, err = ethclient.Dial(Superchains[chain.Superchain].Config.L1.PublicRPC)
+	require.NoError(t, err, "Failed to connect to the Ethereum client at RPC url %s", chain.PublicRPC)
+	defer client.Close()
+
+	got, err = getBool("isCustomGasToken()", Addresses[chain.ChainID].SystemConfigProxy, client)
+	if !strings.Contains(err.Error(), "execution reverted") {
+		// Pre: Custom Gas Token feature. Reverting is acceptable.
+		require.NoError(t, err)
+	} else {
+		// Post: Custom Gas Token fearure. Must be set to false.
+		require.False(t, got)
+	}
 }
 
-func getString(method string, contractAddress Address, client *ethclient.Client) (interface{}, error) {
+func getString(method string, contractAddress Address, client *ethclient.Client) (string, error) {
 	addr := (common.Address(contractAddress))
 	callMsg := ethereum.CallMsg{
 		To:   &addr,
@@ -44,5 +69,37 @@ func getString(method string, contractAddress Address, client *ethclient.Client)
 	result, err := Retry(callContract)(callMsg)
 
 	return common.Bytes2Hex(result), err
+}
 
+func getBool(method string, contractAddress Address, client *ethclient.Client) (bool, error) {
+	addr := (common.Address(contractAddress))
+	callMsg := ethereum.CallMsg{
+		To:   &addr,
+		Data: crypto.Keccak256([]byte(method))[:4],
+	}
+
+	// Make the call
+	callContract := func(msg ethereum.CallMsg) ([]byte, error) {
+		return client.CallContract(context.Background(), msg, nil)
+	}
+
+	result, err := Retry(callContract)(callMsg)
+
+	if err != nil {
+		return false, err
+	}
+
+	// Decode the result
+	var decodedBool bool
+
+	// If the result is a string "0x1" or "0x0", convert to bool
+	if string(result) == "0x1" {
+		decodedBool = true
+	} else if string(result) == "0x0" {
+		decodedBool = false
+	} else {
+		return false, errors.New("Unexpected return value, cannot convert to bool")
+	}
+
+	return decodedBool, nil
 }
