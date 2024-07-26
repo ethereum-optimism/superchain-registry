2a57b461a56422c787ebb0a0d9467dc820417069
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-07-26 12:25:54 -0400

    confirm BatchSubmitter tx.To == BatchInboxAddr

--- validation/data-availability-type_test.go
+++ validation/data-availability-type_test.go
@@ -2,6 +2,7 @@ package validation
 
 import (
 	"context"
+	"math/big"
 	"testing"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -27,6 +29,9 @@ func testDataAvailabilityType(t *testing.T, chain *ChainConfig) {
 	batchSubmitterAddress := Addresses[chain.ChainID].BatchSubmitter
 	require.NotZero(t, batchSubmitterAddress)
 
+	batchInboxAddress := chain.BatchInboxAddr
+	require.NotZero(t, batchInboxAddress)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
@@ -36,10 +41,26 @@ func testDataAvailabilityType(t *testing.T, chain *ChainConfig) {
 	// First attempt without needing an archive node
 	//  - full nodes keep the last 128 blocks but we use 120 to give a buffer
 	nonArchivalDepth := 120
-	_, found, err := eth.CheckRecentTxs(ctx, client, nonArchivalDepth, common.Address(batchSubmitterAddress))
+	blockNum, found, err := eth.CheckRecentTxs(ctx, client, nonArchivalDepth, common.Address(batchSubmitterAddress))
 	if !found {
-		_, found, err = eth.CheckRecentTxs(ctx, client, int(depth), common.Address(batchSubmitterAddress))
+		blockNum, found, err = eth.CheckRecentTxs(ctx, client, int(depth), common.Address(batchSubmitterAddress))
 	}
 	require.NoErrorf(t, err, "failed when checking chain for recent batcher txs from %s", batchSubmitterAddress)
 	require.True(t, found)
+
+	// Fetch the block by number
+	block, err := client.BlockByNumber(context.Background(), new(big.Int).SetUint64(blockNum))
+	require.NoError(t, err)
+
+	// Iterate over the transactions in the block
+	for _, tx := range block.Transactions() {
+		signer := types.NewCancunSigner(tx.ChainId())
+		sender, err := types.Sender(signer, tx)
+		require.NoError(t, err)
+		if sender == common.Address(batchSubmitterAddress) {
+			require.Equal(t, *tx.To(), common.Address(batchInboxAddress))
+			return
+		}
+	}
+	require.Fail(t, "failed to find tx from batch submitter to batch inbox")
 }
