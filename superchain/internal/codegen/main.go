c18e906200a324376df6f32c8fde8ea983b91a28
refcell <abigger87@gmail.com>
2024-08-01 13:30:06 -0400

    fix(codegen): ordering by name instead of chain id

--- superchain/internal/codegen/main.go
+++ superchain/internal/codegen/main.go
@@ -125,7 +125,7 @@ func main() {
 			chainConfigs = append(chainConfigs, *chain)
 		}
 		sort.Slice(chainConfigs, func(i, j int) bool {
-			return chainConfigs[i].ChainID < chainConfigs[j].ChainID
+			return chainConfigs[i].Name < chainConfigs[j].Name
 		})
 		superchains = append(superchains, Superchain{
 			Name:         sc.Superchain,
@@ -134,7 +134,7 @@ func main() {
 		})
 	}
 	sort.Slice(superchains, func(i, j int) bool {
-		return superchains[i].Config.L1.ChainID < superchains[j].Config.L1.ChainID
+		return superchains[i].Config.Name < superchains[j].Config.Name
 	})
 	if len(superchains) != 0 {
 		var buf bytes.Buffer
