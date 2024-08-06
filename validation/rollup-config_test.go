c2a8f01bd5536d574eda4d34b7b518206653cd9b
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-08-06 10:27:29 -0400

    go: rename plasma to altDA

--- validation/rollup-config_test.go
+++ validation/rollup-config_test.go
@@ -10,7 +10,7 @@ import (
 
 func testRollupConfig(t *testing.T, chain *ChainConfig) {
 	standard := std.Config.Params[chain.Superchain].RollupConfig
-	require.Equal(t, chain.Plasma, standard.Plasma, "Standard chains use Ethereum L1 calldata or blobs for data availability (plasma not permitted)")
+	require.Equal(t, chain.AltDA, standard.AltDA, "Standard chains use Ethereum L1 calldata or blobs for data availability (altDA not permitted)")
 	assertIntInBounds(t, "Block Time", chain.BlockTime, standard.BlockTime)
 	assertIntInBounds(t, "Sequencer Window Size", chain.SequencerWindowSize, standard.SequencerWindowSize)
 }
