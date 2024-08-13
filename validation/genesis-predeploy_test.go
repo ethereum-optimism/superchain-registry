6e82c306771e1a07e1bdac152df9b3495f06a510
geoknee <georgeknee@googlemail.com>
2024-08-13 22:25:51 +0100

    wip start extracting bytecode from artifact

--- validation/genesis-predeploy_test.go
+++ validation/genesis-predeploy_test.go
@@ -2,6 +2,7 @@ package validation
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,7 +10,18 @@ import (
 	"path/filepath"
 	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
+
+// Define a struct to represent the structure of the JSON data
+type DeployedBytecode struct {
+	Object string `json:"object"`
+}
+
+type ContractData struct {
+	DeployedBytecode DeployedBytecode `json:"deployedBytecode"`
+}
 
 // REQUIREMENTS:
 // yarn, so we can prepare https://codeload.github.com/Saw-mon-and-Natalie/clones-with-immutable-args/tar.gz/105efee1b9127ed7f6fedf139e1fc796ce8791f2
@@ -29,7 +41,7 @@ func TestGenesisPredeploys(t *testing.T) {
 
 	monorepoCommit := "d80c145e0acf23a49c6a6588524f57e32e33b91c"
 
-	executeCommandInDir(t, monorepoDir, exec.Command("git", "checkout", monorepoCommit))
+	executeCommandInDir(t, monorepoDir, exec.Command("git", "checkout", monorepoCommit)) // could use reset --hard to make it easier to run again
 	executeCommandInDir(t, monorepoDir, exec.Command("git", "fetch", "--recurse-submodules"))
 
 	// TODO unskip these, I am skipping to save time in development
@@ -45,6 +57,14 @@ func TestGenesisPredeploys(t *testing.T) {
 	executeCommandInDir(t, contractsDir, exec.Command("git", "apply", "foundry-config.patch"))
 
 	executeCommandInDir(t, contractsDir, exec.Command("forge", "build"))
+
+	data, err := os.ReadFile(path.Join(contractsDir, "forge-artifacts/BaseFeeVault.sol/BaseFeeVault.json"))
+	require.NoError(t, err)
+
+	cd := new(ContractData)
+	err = json.Unmarshal(data, cd)
+	require.NoError(t, err)
+	t.Log(cd)
 }
 
 func executeCommandInDir(t *testing.T, dir string, cmd *exec.Cmd) {
