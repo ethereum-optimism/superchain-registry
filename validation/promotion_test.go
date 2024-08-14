6c18edf8ca6602c67a0396d8df87daf040d3b4e8
George Knee <georgeknee@googlemail.com>
2024-08-14 18:16:45 +0100

    Introduce TestPromotion (#468)
    * Adds a nightly job to run `just promotion-test` to kick off the new
      command to check promotion candidate failures
    * refactored code to invoke a common `notify-failures-to-main`, invoked
      with slack channel ID as parameter
    * Change output format and exclude promotion check test from other runs
    * change output format to `dots`, only test failures are logged. this is
      a concise output for directing failures to new slack channel.
    * exclude `promotion-test` from the regular validation-test run
    * Tweak test assertion check for better output
    * Better handling for comparing addresses
    
    Co-authored-by: Vinod Damle <vinod@oplabs.co>

---
+++ validation/promotion_test.go
@@ -0,0 +1,26 @@
+package validation
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/ethereum-optimism/superchain-registry/superchain"
+)
+
+func TestPromotion(t *testing.T) {
+	for _, chain := range OPChains {
+		chain := chain
+		t.Run(perChainTestName(chain), func(t *testing.T) {
+			t.Parallel()
+			if chain.StandardChainCandidate {
+				// promote the chain to standard
+				// by mutating the chainConfig
+				chain.StandardChainCandidate = false
+				chain.SuperchainLevel = Standard
+				now := uint64(time.Now().Unix())
+				chain.SuperchainTime = &now
+				testStandard(t, chain)
+			}
+		})
+	}
+}
