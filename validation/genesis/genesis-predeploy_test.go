b2c7111aa5f496e2993755edd629e3c78493c8f8
geoknee <georgeknee@googlemail.com>
2024-08-15 17:38:22 +0100

    complete validation of mode predeploy bytecodes

--- validation/genesis/genesis-predeploy_test.go
+++ validation/genesis/genesis-predeploy_test.go
@@ -3,6 +3,7 @@ package genesis
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -49,24 +50,24 @@ type GenesisLite struct {
 func TestGenesisPredeploys(t *testing.T) {
 
 	artifactNames := map[string]string{
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30013": "forge-artifacts/L1FeeVault.sol/L1FeeVault.json",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30007": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30017": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30011": "",
-		// "0x4200000000000000000000000000000000000042": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3000f": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30014": "",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30013": "L1BlockNumber",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30007": "L2CrossDomainMessenger",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30017": "OptimismMintableERC721Factory",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30011": "SequencerFeeVault",
+		"0x4200000000000000000000000000000000000042": "GovernanceToken",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3000f": "GasPriceOracle",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30014": "L2ERC721Bridge",
 		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3001a": "L1FeeVault",
 		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30019": "BaseFeeVault",
 		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30020": "SchemaRegistry",
 		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30021": "EAS",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30012": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30018": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30002": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30015": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30016": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30010": "",
-		// "0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30000": "",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30012": "OptimismMintableERC20Factory",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30018": "ProxyAdmin",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30002": "DeployerWhitelist", // Deprecated according to specs
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30015": "L1Block",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30016": "L2ToL1MessagePasser",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30010": "L2StandardBridge",
+		"0xc0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d3c0d30000": "LegacyMessagePasser", // Deprecated according to specs
 	}
 
 	_, filename, _, ok := runtime.Caller(0)
@@ -123,11 +124,20 @@ func TestGenesisPredeploys(t *testing.T) {
 			address = "0x" + address
 		}
 
+		if address == "0x4200000000000000000000000000000000000042" {
+			// GovernanceToken should NOT be set in any standard chain
+			// But it is for OP mainnet, hence the need to special case this one
+			// TODO instate a proper check here
+			// I think sometimes this reverts to an ordinary proxy when governance token is disabled
+			// see https://www.notion.so/oplabs/BHIC-L2-Genesis-Predeploy-Verification-Metal-Mode-Zora-f42ecf4dd0164b4d9303b933d9e98bf6#0a94fd37ca37474d998ab1668e655264
+			continue
+		}
+
 		// Note that although the spec states this is a one-byte (two hexit) namespace, in fact we are using up to three hexits
 		// e.g. 4200000000000000000000000000000000000800 is present in the expected genesis
 		// Note also we don't want to skip
 		if strings.HasPrefix(address, "0x4200000000000000000000000000000000000") && address != "0x4200000000000000000000000000000000000042" {
-			// TODO for now we will skip the proxies themselves (but lets not skip unproxied predeploys)
+			// TODO for now we will skip the proxies themselves (but lets not skip unproxied predeploys here)
 			continue
 		}
 
@@ -159,23 +169,43 @@ func TestGenesisPredeploys(t *testing.T) {
 		require.NoError(t, err)
 
 		// TODO check if this is already equal, in which case masking is not necessary
-		maskBytecode(gotByteCode, cd.DeployedBytecode.ImmutableReferences)
+		err = maskBytecode(gotByteCode, cd.DeployedBytecode.ImmutableReferences)
+		if err != nil {
+			t.Errorf("err masking bytecode for %s, %s", address, err)
+		}
 		gotByteCodeHex := hexutil.Encode(gotByteCode)
 
 		require.Equal(t, wantByteCodeHex, gotByteCodeHex, "address %s failed validation!", address)
-		t.Log(address + " OK!\n")
+
+		// Just realised that the Semver universal contract used immutables in the past, making immutables far more prolific (due the semver contract
+		// being inherited by many other contracts)
+		// These would not be security critical immutables, however, since they can't be changed without modifying the rest of the inherit_ing_ contracts bytecode
+		// so our "mask and check" validation approach covers us well.
+		t.Logf(address+" ✅ OK! (%s with %d immutable references)", artifactName, countImmutables(cd.DeployedBytecode.ImmutableReferences))
 
 	}
 }
 
-func maskBytecode(b []byte, immutableReferences map[string][]ImmutableReference) {
+func countImmutables(irs map[string][]ImmutableReference) int {
+	count := 0
+	for range irs {
+		count++
+	}
+	return count
+}
+
+func maskBytecode(b []byte, immutableReferences map[string][]ImmutableReference) error {
 	for _, v := range immutableReferences {
 		for _, r := range v {
 			for i := r.Start; i < r.Start+r.Length; i++ {
+				if i >= len(b) {
+					return errors.New("immutable references extend beyond bytecode")
+				}
 				b[i] = 0
 			}
 		}
 	}
+	return nil
 }
 
 func executeCommandInDir(t *testing.T, dir string, cmd *exec.Cmd) {
