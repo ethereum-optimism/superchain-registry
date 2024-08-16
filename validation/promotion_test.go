d769dfe4c896e05cb7f4821217b91cc01044cc03
geoknee <georgeknee@googlemail.com>
2024-08-16 11:16:53 +0100

    harden up TestPromotion

--- validation/promotion_test.go
+++ validation/promotion_test.go
@@ -13,12 +13,15 @@ func TestPromotion(t *testing.T) {
 		t.Run(perChainTestName(chain), func(t *testing.T) {
 			t.Parallel()
 			if chain.StandardChainCandidate {
+				// do not allow any test exclusions
+				exclusions = nil
 				// promote the chain to standard
 				// by mutating the chainConfig
 				chain.StandardChainCandidate = false
 				chain.SuperchainLevel = Standard
 				now := uint64(time.Now().Unix())
 				chain.SuperchainTime = &now
+				testStandardCandidate(t, chain)
 				testStandard(t, chain)
 			}
 		})
