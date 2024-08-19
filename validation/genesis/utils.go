6b600b2f604d8b504c1eda6b5fd2cb21c36c76e9
geoknee <georgeknee@googlemail.com>
2024-08-19 19:21:40 +0100

    add genesis predeploy test, validation metadata for chains 10,34443 and wire up

---
+++ validation/genesis/utils.go
@@ -0,0 +1,23 @@
+package genesis
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func executeCommandInDir(t *testing.T, dir string, cmd *exec.Cmd) {
+	t.Logf("executing %s", cmd.String())
+	cmd.Dir = dir
+	var outErr bytes.Buffer
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = &outErr
+	err := cmd.Run()
+	if err != nil {
+		// error case : status code of command is different from 0
+		fmt.Println(outErr.String())
+		t.Fatal(err)
+	}
+}
