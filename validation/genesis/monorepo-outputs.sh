95409a10a9703a40dfaf1ba16a655231146fdc64
geoknee <georgeknee@googlemail.com>
2024-08-28 18:13:44 +0100

    use . instead of source

--- validation/genesis/monorepo-outputs.sh
+++ validation/genesis/monorepo-outputs.sh
@@ -12,11 +12,9 @@ echo "Installing and selecting correct go version"
 go_version=$(grep -m 1 '^go ' go.mod | awk '{print $2}')
 
 # Source the gvm script to load gvm functions into the shell
-set +e
-source ~/.gvm/scripts/gvm || exit 1
+. ~/.gvm/scripts/gvm || exit 1
 gvm install go${go_version} || exit 1
 gvm use go${go_version} || exit 1
-set -e
 
 echo "Running op-node genesis l2 command"
 eval "$3"
