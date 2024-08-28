d3afe5340700b7b5f877c748c92b75470f64dabf
geoknee <georgeknee@googlemail.com>
2024-08-28 13:29:16 +0100

    switch to cimg/go:1.22.6-node docker image for genesis validation

--- validation/genesis/monorepo-outputs.sh
+++ validation/genesis/monorepo-outputs.sh
@@ -1,15 +1,16 @@
 set -e
 
-echo "Inferring and selecting correct Node version"
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+echo "Installing and selecting correct Node version"
+nvm install $1
 nvm use $1
 
 echo "Running install command"
 eval $2
 
-echo "Inferring and selecting correct go version"
-
+echo "Installing and selecting correct go version"
 go_version=$(grep -m 1 '^go ' go.mod | awk '{print $2}')
 
 # Source the gvm script to load gvm functions into the shell
@@ -19,7 +20,5 @@ gvm install go${go_version} || exit 1
 gvm use go${go_version} || exit 1
 set -e
 
-
 echo "Running op-node genesis l2 command"
-
 eval "$3"
