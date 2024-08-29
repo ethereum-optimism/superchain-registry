25649b6922d360c78a1873fa09b3df9a56b25260
geoknee <georgeknee@googlemail.com>
2024-08-29 10:25:28 +0100

    improve pipefail behaviour
    we allow the scripts which load nvm and gvm to fail

--- validation/genesis/monorepo-outputs.sh
+++ validation/genesis/monorepo-outputs.sh
@@ -1,6 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
+set -e
 echo "Installing and selecting correct Node version"
 nvm install $1
 nvm use $1
@@ -12,9 +13,11 @@ echo "Installing and selecting correct go version"
 go_version=$(grep -m 1 '^go ' go.mod | awk '{print $2}')
 
 # Source the gvm script to load gvm functions into the shell
-. ~/.gvm/scripts/gvm || exit 1
-gvm install go${go_version} || exit 1
-gvm use go${go_version} || exit 1
+set +e
+. ~/.gvm/scripts/gvm
+set -e
+gvm install go${go_version}
+gvm use go${go_version}
 
 echo "Running op-node genesis l2 command"
 eval "$3"
