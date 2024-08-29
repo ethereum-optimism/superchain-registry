703267d771e05d7210efe9d0846b3aeaae200223
geoknee <georgeknee@googlemail.com>
2024-08-29 10:50:53 +0100

    stream output from test
    helps with debugging, prevents timeouts on circleci

--- validation/genesis/genesis-predeploy_test.go
+++ validation/genesis/genesis-predeploy_test.go
@@ -1,8 +1,10 @@
 package genesis
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -110,7 +112,22 @@ func testGenesisPredeploys(t *testing.T, chain *ChainConfig) {
 		panic("must set node_version in meta.toml")
 	}
 	creationCommand := GenesisCreationCommand[vis.GenesisCreationCommand](chainId, Superchains[chain.Superchain].Config.L1.PublicRPC)
-	mustExecuteCommandInDir(monorepoDir, exec.Command("bash", "./monorepo-outputs.sh", vis.NodeVersion, buildCommand, creationCommand))
+	cmd := exec.Command("bash", "./monorepo-outputs.sh", vis.NodeVersion, buildCommand, creationCommand)
+	// Create a pipe to read the command's output
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		t.Fatalf("Failed to get stdout pipe: %v", err)
+	}
+
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		t.Fatalf("Failed to get stderr pipe: %v", err)
+	}
+	// Stream the command's stdout and stderr to the test logger
+	go streamOutputToLogger(stdoutPipe, t)
+	go streamOutputToLogger(stderrPipe, t)
+
+	mustExecuteCommandInDir(monorepoDir, cmd)
 
 	expectedData, err := os.ReadFile(path.Join(monorepoDir, "expected-genesis.json"))
 	require.NoError(t, err)
@@ -158,6 +175,16 @@ func writeDeployments(chainId uint64, directory string) error {
 		return err
 	}
 	return nil
+}
+
+func streamOutputToLogger(reader io.Reader, t *testing.T) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		t.Log(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		t.Errorf("Error reading command output: %v", err)
+	}
 }
 
 func writeDeploymentsLegacy(chainId uint64, directory string) error {
