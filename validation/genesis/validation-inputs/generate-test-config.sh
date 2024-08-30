d7b4b1903dd2bb1723f9e53587ff0e03d29bc6c4
geoknee <georgeknee@googlemail.com>
2024-08-30 14:56:05 +0100

    fix script

--- validation/genesis/validation-inputs/generate-test-config.sh
+++ validation/genesis/validation-inputs/generate-test-config.sh
@@ -38,5 +38,5 @@ prependedTargets="[$prependedTargets]"
 brew install yq
 
 # Use yq to replace the target-version   key
-yq e ".workflows.pr-checks.jobs[0].golang-validate-genesis-allocs.matrix.parameters.\"chainid\" = $targets" -i .circleci/continue_config.yml
-yq e ".workflows.pr-checks.jobs[0].genesis-allocs-all-ok.requires = $prependedTargets" -i .circleci/continue_config.yml
+yq e ".workflows.pr-checks.jobs[0].golang-validate-genesis-allocs.matrix.parameters.chainid = $targets" -i .circleci/continue_config.yml
+yq e ".workflows.pr-checks.jobs[1].genesis-allocs-all-ok.requires = $prependedTargets" -i .circleci/continue_config.yml
