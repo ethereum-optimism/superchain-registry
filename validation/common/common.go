fae5707d28ce610c9db16ea60acc6ae874031733
geoknee <georgeknee@googlemail.com>
2024-08-30 15:19:18 +0100

    lint

--- validation/common/common.go
+++ validation/common/common.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/ethereum-optimism/superchain-registry/superchain"
 )
+
 // PerChainTestName ensures test can easily be filtered by chain name or chain id using the -run=regex testflag.
 func PerChainTestName(chain *ChainConfig) string {
 	return chain.Name + fmt.Sprintf(" (%d)", chain.ChainID)
