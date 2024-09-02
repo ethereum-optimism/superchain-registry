4d665cb888cdf5e2d2b9f5d6433e88ad4ab2ea04
geoknee <georgeknee@googlemail.com>
2024-09-02 16:50:56 +0100

    try removing gvm
    Go is backwards compatible, it should just work...

--- validation/genesis/monorepo-outputs.sh
+++ validation/genesis/monorepo-outputs.sh
@@ -8,13 +8,5 @@ nvm use $1
 echo "Running install command"
 eval $2
 
-echo "Installing and selecting correct go version"
-go_version=$(grep -m 1 '^go ' go.mod | awk '{print $2}')
-
-# Source the gvm script to load gvm functions into the shell
-. ~/.gvm/scripts/gvm
-gvm install go${go_version}
-gvm use go${go_version}
-
 echo "Running op-node genesis l2 command"
 eval "$3"
