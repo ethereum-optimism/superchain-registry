e3b99ceaba262442a476f23a6b9c8fb032dd80f1
geoknee <georgeknee@googlemail.com>
2024-09-02 16:59:00 +0100

    add note about gvm

--- validation/genesis/monorepo-outputs.sh
+++ validation/genesis/monorepo-outputs.sh
@@ -12,6 +12,10 @@ echo "Installing and selecting correct go version"
 go_version=$(grep -m 1 '^go ' go.mod | awk '{print $2}')
 
 # Source the gvm script to load gvm functions into the shell
+# NOTE gvm is necessary because older versions of op-node used a
+# library which is not compatible with newer versions of Go
+# Running with go1.21 results in
+# The version of quic-go you're using can't be built on Go 1.21 yet. For more details, please see https://github.com/quic-go/quic-go/wiki/quic-go-and-Go-versions."
 . ~/.gvm/scripts/gvm
 gvm install go${go_version}
 gvm use go${go_version}
