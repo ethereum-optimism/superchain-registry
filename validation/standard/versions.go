54aea60c4583655971386fd3b7fa45e72ad8b90a
Blaine Malone <blainemalone01@gmail.com>
2024-09-26 09:41:32 -0400

    feat: Adding Sepolia standard versions (#615)
    * feat: Adding Sepolia standard versions
    
    * fix: hack to get compiling
    
    * fix: removed accidentally added submodule.
    
    * fix: using ChaingConfig
    
    * fix: added sepolia addresses for op-contracts/v1.6.0
    
    * validation: tidy up binding of standard release and standard immutable references (#616)
    
    * hoist standard_release to its own file
    
    * tidy up binding of standard release and immutableReferences
    
    * just lint-all
    
    * remove unnecessary condition
    
    * typo
    
    ---------
    
    Co-authored-by: George Knee <georgeknee@googlemail.com>

--- validation/standard/versions.go
+++ validation/standard/versions.go
@@ -8,20 +8,51 @@ import (
 
 type Tag string
 
+// ContractBytecodeImmutables stores the immutable references as a raw stringified JSON string in a TOML config.
+// it is stored this way because it can be plucked out of the contract compilation output as is and pasted into the TOML config file.
+type ContractBytecodeImmutables struct {
+	AnchorStateRegistry string `toml:"anchor_state_registry,omitempty"`
+	DelayedWETH         string `toml:"delayed_weth,omitempty"`
+	FaultDisputeGame    string `toml:"fault_dispute_game,omitempty"`
+	MIPS                string `toml:"mips,omitempty"`
+}
+
+func (c ContractBytecodeImmutables) ForContractWithName(name string) (string, bool) {
+	// Use reflection to get the struct value and type
+	v := reflect.ValueOf(c)
+
+	// Try to find the field by name
+	field := v.FieldByName(name)
+	if !field.IsValid() {
+		return "", false
+	}
+
+	// Check if the field is of type string
+	if field.Type() != reflect.TypeOf("") {
+		return "", false
+	}
+
+	// Check if the string is empty
+	s := field.Interface().(string)
+	if s == "" {
+		return "", false
+	}
+
+	return s, true
+}
+
 type (
 	BytecodeHashTags       = map[Tag]L1ContractBytecodeHashes
 	BytecodeImmutablesTags = map[Tag]ContractBytecodeImmutables
 )
 
 type VersionTags struct {
-	Releases        map[Tag]superchain.ContractVersions `toml:"releases"`
-	StandardRelease Tag                                 `toml:"standard_release,omitempty"`
+	Releases map[Tag]superchain.ContractVersions `toml:"releases"`
 }
 
 var (
-	Versions VersionTags = VersionTags{
-		Releases: make(map[Tag]superchain.ContractVersions, 0),
-	}
+	Release            Tag
+	NetworkVersions                           = make(map[string]VersionTags)
 	BytecodeHashes     BytecodeHashTags       = make(BytecodeHashTags, 0)
 	BytecodeImmutables BytecodeImmutablesTags = make(BytecodeImmutablesTags, 0)
 )
