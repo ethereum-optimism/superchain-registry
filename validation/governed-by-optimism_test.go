c24680f45bded8a8768cbb3b6125218a08cdc08e
Eugene Dobry <eugene@dobry.me>
2024-11-27 16:12:53 -0500

    add GovernedByOptimismTest

---
+++ validation/governed-by-optimism_test.go
@@ -0,0 +1,22 @@
+package validation
+
+import (
+	"testing"
+
+	. "github.com/ethereum-optimism/superchain-registry/superchain"
+	"github.com/ethereum-optimism/superchain-registry/validation/standard"
+	// "github.com/ethereum/go-ethereum/common"
+
+	"github.com/stretchr/testify/require"
+)
+
+func testGovernedByOptimism(t *testing.T, chain *ChainConfig) {
+	chainID := chain.ChainID
+	superchain := OPChains[chainID].Superchain
+
+	optimismMultisig := standard.Config.MultisigRoles[superchain].KeyHandover.L1.Universal["ProxyAdmin"]["owner()"]
+	
+    if chain.GovernedByOptimism == true {
+		require.Equal(t, chain.Addresses.ProxyAdmin.String(), optimismMultisig, "Chains using Optimism governance must have their ProxyAdmin set to the Optimism multisig")
+	}
+}
