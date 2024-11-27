f02bc6b339310cca31bcdfebd40e332e80020c78
Eugene Dobry <eugene@dobry.me>
2024-11-27 16:36:23 -0500

    fix lint errors

--- add-chain/main.go
+++ add-chain/main.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-    "fmt"
-    "os"
 	"flag"
+	"fmt"
+	"os"
 
-    "github.com/ethereum-optimism/superchain-registry/add-chain/cmd"
-    "github.com/joho/godotenv"
-    "github.com/urfave/cli/v2"
+	"github.com/ethereum-optimism/superchain-registry/add-chain/cmd"
+	"github.com/joho/godotenv"
+	"github.com/urfave/cli/v2"
 )
 
 var app = &cli.App{
-    Name:  "add-chain",
-    Usage: "Utilities for working with the superchain-registry",
-    Action: entrypoint,
-    Commands: []*cli.Command{
-        &cmd.AddChainCmd,
-        &cmd.PromoteToStandardCmd,
-        &cmd.CheckRollupConfigCmd,
-        &cmd.CompressGenesisCmd,
-        &cmd.CheckGenesisCmd,
-        &cmd.UpdateConfigsCmd,
-    },
+	Name:   "add-chain",
+	Usage:  "Utilities for working with the superchain-registry",
+	Action: entrypoint,
+	Commands: []*cli.Command{
+		&cmd.AddChainCmd,
+		&cmd.PromoteToStandardCmd,
+		&cmd.CheckRollupConfigCmd,
+		&cmd.CompressGenesisCmd,
+		&cmd.CheckGenesisCmd,
+		&cmd.UpdateConfigsCmd,
+	},
 }
 
 func main() {
-    if err := runApp(os.Args); err != nil {
-        fmt.Println(err)
-        fmt.Println("*********************")
-        fmt.Printf("FAILED: %s\n", app.Name)
-        os.Exit(1)
-    }
-    fmt.Println("*********************")
-    fmt.Printf("SUCCESS: %s\n", app.Name)
+	if err := runApp(os.Args); err != nil {
+		fmt.Println(err)
+		fmt.Println("*********************")
+		fmt.Printf("FAILED: %s\n", app.Name)
+		os.Exit(1)
+	}
+	fmt.Println("*********************")
+	fmt.Printf("SUCCESS: %s\n", app.Name)
 }
 
 func runApp(args []string) error {
-    // Load the appropriate .env file
-    var err error
-    if runningTests := os.Getenv("SCR_RUN_TESTS"); runningTests == "true" {
-        fmt.Println("Loading .env.test")
-        err = godotenv.Load("./testdata/.env.test")
-    } else {
-        err = godotenv.Load()
-    }
+	// Load the appropriate .env file
+	var err error
+	if runningTests := os.Getenv("SCR_RUN_TESTS"); runningTests == "true" {
+		fmt.Println("Loading .env.test")
+		err = godotenv.Load("./testdata/.env.test")
+	} else {
+		err = godotenv.Load()
+	}
 
-    if err != nil && !os.IsNotExist(err) {
-        panic("error loading .env file")
-        fmt.Println(err)
-    }
+	if err != nil && !os.IsNotExist(err) {
+		fmt.Println(err)
+		panic("error loading .env file")
+	}
 
-    return app.Run(args)
+	return app.Run(args)
 }
 
 // call default command (AddChainCmd) if no subcommand is provided
 func entrypoint(c *cli.Context) error {
-     if c.Args().Present() {
-        // Unknown command or arguments were provided
-        return cli.ShowAppHelp(c)
-    }
+	if c.Args().Present() {
+		// Unknown command or arguments were provided
+		return cli.ShowAppHelp(c)
+	}
 
-    // No subcommand provided; invoke the default command
+	// No subcommand provided; invoke the default command
 
-    // Create a new flag set for the default command
-    set := flag.NewFlagSet(cmd.AddChainCmd.Name, flag.ExitOnError)
-    for _, f := range cmd.AddChainCmd.Flags {
-        f.Apply(set)
-    }
+	// Create a new flag set for the default command
+	set := flag.NewFlagSet(cmd.AddChainCmd.Name, flag.ExitOnError)
+	for _, f := range cmd.AddChainCmd.Flags {
+		_ = f.Apply(set)
+	}
 
-    // Create a new context with the new flag set
-    ctx := cli.NewContext(c.App, set, c)
+	// Create a new context with the new flag set
+	ctx := cli.NewContext(c.App, set, c)
 
-    // Set the command to the default command
-    ctx.Command = &cmd.AddChainCmd
+	// Set the command to the default command
+	ctx.Command = &cmd.AddChainCmd
 
-    // Invoke the default command's Run method
-    return cmd.AddChainCmd.Run(ctx)
+	// Invoke the default command's Run method
+	return cmd.AddChainCmd.Run(ctx)
 }
