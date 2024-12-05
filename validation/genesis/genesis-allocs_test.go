f68281c439d188ebf4caf6c558b0c4eedc1e0b16
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-12-04 22:42:15 -0500

    validation: add ink sepolia genesis-allocs exceptions

--- validation/genesis/genesis-allocs_test.go
+++ validation/genesis/genesis-allocs_test.go
@@ -184,6 +184,21 @@ func testGenesisAllocs(t *testing.T, chain *ChainConfig) {
 		delete(g.Alloc, common.HexToAddress("0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f"))
 	}
 
+	// Ink Sepolia
+	if chainId == uint64(763373) {
+		// forge deployer address
+		delete(g.Alloc, common.HexToAddress("0xae0bdc4eeac5e950b67c6819b118761caaf61946"))
+		// GovernanceToken predeploy storage slot was set to null, which is functionally equivalent to the 0x000...dead address
+		governanceTokenAddress := common.HexToAddress("0x4200000000000000000000000000000000000042")
+		storageSlot := common.HexToHash("0x000000000000000000000000000000000000000000000000000000000000000a")
+		if _, ok := g.Alloc[governanceTokenAddress].
+			Storage[storageSlot]; ok {
+			require.Fail(t, "expected GovernanceToken contract storage slot to be null")
+		}
+		g.Alloc[governanceTokenAddress].
+			Storage[storageSlot] = common.HexToHash("0x000000000000000000000000deaddeaddeaddeaddeaddeaddeaddeaddeaddead")
+	}
+
 	gotData, err := json.MarshalIndent(g.Alloc, "", " ")
 	require.NoError(t, err)
 
