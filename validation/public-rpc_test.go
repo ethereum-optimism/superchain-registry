de3e72f35a2184fb18f3f5ec48b19187ada5d4c1
Eugene Dobry <eugene@dobry.me>
2024-12-13 14:50:33 -0500

    add PublicRPCTest

---
+++ validation/public-rpc_test.go
@@ -0,0 +1,18 @@
+package validation
+
+import (
+	"testing"
+
+	. "github.com/ethereum-optimism/superchain-registry/superchain"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/require"
+)
+
+func testPublicRPC(t *testing.T, chain *ChainConfig) {
+	rpcEndpoint := chain.PublicRPC
+	require.NotEmpty(t, rpcEndpoint, "no public_rpc endpoint specified")
+
+	client, err := ethclient.Dial(rpcEndpoint)
+	defer client.Close()
+	require.NoErrorf(t, err, "could not dial rpc endpoint '%s'", rpcEndpoint)
+}
