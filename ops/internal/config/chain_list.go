01efaad00330fc8ccadac954dc0f469c24d64096
Samuel Stokes <sam.adam.stokes@gmail.com>
2025-04-04 14:33:33 -0400

    transform op-fetcher proofs struct to unique chainList struct

--- ops/internal/config/chain_list.go
+++ ops/internal/config/chain_list.go
@@ -1,19 +1,21 @@
 package config
 
-import "github.com/ethereum-optimism/optimism/op-fetcher/pkg/fetcher/fetch/script"
-
 type ChainListEntry struct {
-	Name                 string                   `json:"name" toml:"name"`
-	Identifier           string                   `json:"identifier" toml:"identifier"`
-	ChainID              uint64                   `json:"chainId" toml:"chain_id"`
-	RPC                  []string                 `json:"rpc" toml:"rpc"`
-	Explorers            []string                 `json:"explorers" toml:"explorers"`
-	SuperchainLevel      SuperchainLevel          `json:"superchainLevel" toml:"superchain_level"`
-	GovernedByOptimism   bool                     `json:"governedByOptimism" toml:"governed_by_optimism"`
-	DataAvailabilityType string                   `json:"dataAvailabilityType" toml:"data_availability_type"`
-	Parent               ChainListEntryParent     `json:"parent" toml:"parent"`
-	GasPayingToken       *ChecksummedAddress      `json:"gasPayingToken,omitempty" toml:"gas_paying_token,omitempty"`
-	FaultProofStatus     *script.FaultProofStatus `json:"faultProofs,omitempty" toml:"fault_proofs,omitempty"`
+	Name                 string               `json:"name" toml:"name"`
+	Identifier           string               `json:"identifier" toml:"identifier"`
+	ChainID              uint64               `json:"chainId" toml:"chain_id"`
+	RPC                  []string             `json:"rpc" toml:"rpc"`
+	Explorers            []string             `json:"explorers" toml:"explorers"`
+	SuperchainLevel      SuperchainLevel      `json:"superchainLevel" toml:"superchain_level"`
+	GovernedByOptimism   bool                 `json:"governedByOptimism" toml:"governed_by_optimism"`
+	DataAvailabilityType string               `json:"dataAvailabilityType" toml:"data_availability_type"`
+	Parent               ChainListEntryParent `json:"parent" toml:"parent"`
+	GasPayingToken       *ChecksummedAddress  `json:"gasPayingToken,omitempty" toml:"gas_paying_token,omitempty"`
+	FaultProofs          FaultProofs          `json:"faultProofs,omitempty" toml:"fault_proofs,omitempty"`
+}
+
+type FaultProofs struct {
+	Status string `json:"status" toml:"status"`
 }
 
 type ChainListEntryParent struct {
