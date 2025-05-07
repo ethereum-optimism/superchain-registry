c18a8df289dd74db6ae478547e7deeae440aee0d
George Knee <georgeknee@googlemail.com>
2025-05-07 18:15:46 +0100

    wip

--- ops/cmd/bbfs/go.mod
+++ ops/cmd/bbfs/go.mod
@@ -4,4 +4,37 @@ go 1.23.1
 
 require github.com/BurntSushi/toml v1.5.0
 
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/bits-and-blooms/bitset v1.20.0 // indirect
+	github.com/consensys/bavard v0.1.27 // indirect
+	github.com/consensys/gnark-crypto v0.16.0 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.1.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.15.11 // indirect
+	github.com/ethereum/go-verkle v0.2.2 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/lmittmann/w3 v0.19.5 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/naoina/go-stringutil v0.1.0 // indirect
+	github.com/naoina/toml v0.1.2-0.20170918210437-9fafd6967416 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/supranational/blst v0.3.14 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
+
 replace github.com/ethereum/go-ethereum => github.com/ethereum-optimism/op-geth v1.101503.2-rc.5.0.20250401164435-02dfe8692a3c
--- ops/cmd/bbfs/main.go
+++ ops/cmd/bbfs/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 // Minimal struct to unmarshal relevant fields
@@ -18,17 +25,40 @@ type ChainConfig struct {
 	} `toml:"addresses"`
 }
 
-func main() {
+// ABI definition for the blobBaseFeeScalar function
+const blobBaseFeeScalarABI = `[{"constant":true,"inputs":[],"name":"blobbasefeeScalar","outputs":[{"name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"}]`
 
-	data := map[int]ChainConfig{}
+func main() {
+	// RPC URL for Optimism Mainnet
+	rpcURL := "https://ethereum-rpc.publicnode.com"
+
+	// Connect to the Ethereum client
+	client, err := ethclient.Dial(rpcURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to RPC: %v", err)
+	}
+	defer client.Close()
 
 	// Relative path to the superchain registry
-	relativePath := "../../.."
-
-	// Convert to an absolute path
+	relativePath := "../../../superchain/configs/mainnet"
 	rootDir, err := filepath.Abs(relativePath)
 	if err != nil {
-		log.Fatalf("Error converting path to absolute: %v\n", err)
+		log.Fatalf("Error converting path to absolute: %v", err)
+	}
+
+	// Check if the directory exists
+	if _, err := os.Stat(rootDir); os.IsNotExist(err) {
+		log.Fatalf("Error: directory does not exist: %s", rootDir)
+	}
+
+	fmt.Printf("Using Superchain Registry directory: %s\n", rootDir)
+	fmt.Printf("%-30s | %-10s | %-42s | %-20s | %s \n", "Chain Name", "Chain ID", "SystemConfigProxy", "blobbasefeeScalar", "reverted")
+	fmt.Println(strings.Repeat("-", 115))
+
+	// Parse the ABI
+	parsedABI, err := abi.JSON(strings.NewReader(blobBaseFeeScalarABI))
+	if err != nil {
+		log.Fatalf("Failed to parse ABI: %v", err)
 	}
 
 	// Walk through the file tree
@@ -40,34 +70,59 @@ func main() {
 
 		// Process only TOML files
 		if filepath.Ext(path) == ".toml" {
-			// Read the TOML file
-			content, err := os.ReadFile(path)
-			if err != nil {
-				log.Printf("Error reading file %s: %v\n", path, err)
-				return nil
+
+			if filepath.Base(path) == "superchain.toml" {
+				return nil // Skip the superchain.toml file
 			}
 
-			// Unmarshal the TOML content into the struct
 			var config ChainConfig
-			if err := toml.Unmarshal(content, &config); err != nil {
+
+			// Unmarshal the TOML content into the struct
+			if _, err := toml.DecodeFile(path, &config); err != nil {
 				log.Printf("Error unmarshalling TOML in %s: %v\n", path, err)
 				return nil
 			}
 
-			// Skip files that don't have the required fields
-			if config.Name == "" || config.ChainID == 0 || config.Addresses.SystemConfigProxy == "" {
-				log.Printf("Skipping incomplete TOML file: %s\n", path)
+			// Validate address format
+			if len(config.Addresses.SystemConfigProxy) != 42 || !strings.HasPrefix(config.Addresses.SystemConfigProxy, "0x") {
+				log.Printf("Invalid address format in %s: %s\n", path, config.Addresses.SystemConfigProxy)
 				return nil
 			}
 
-			// Print the extracted information
-			fmt.Printf("Chain Name: %s\nChain ID: %d\nSystemConfigProxy: %s\n",
-				config.Name, config.ChainID, config.Addresses.SystemConfigProxy)
-			fmt.Println("------------------------------------------------")
+			// Convert the address to a common.Address type
+			address := common.HexToAddress(config.Addresses.SystemConfigProxy)
 
-			data[config.ChainID] = config
+			// Prepare the call to the blobBaseFeeScalar function
+
+			callData, err := hexutil.Decode("0xec707517") // This is the method ID for blobbasefeeScalar
+			if err != nil {
+				log.Printf("Error decoding call data for %s: %v\n", address.Hex(), err)
+				return nil
+			}
+			// Make the contract call
+			var reverted bool
+			result, err := client.CallContract(context.Background(), ethereum.CallMsg{
+				To:   &address,
+				Data: callData,
+			}, nil)
+			if err != nil {
+				reverted = true
+			}
+
+			// Unpack the result
+			var blobBaseFeeScalar uint32
+
+			if !reverted {
+				err = parsedABI.UnpackIntoInterface(&blobBaseFeeScalar, "blobbasefeeScalar", result)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			// Print the result
+			fmt.Printf("%-30s | %-10d | %-42s | %-20d | %t\n",
+				config.Name, config.ChainID, config.Addresses.SystemConfigProxy, blobBaseFeeScalar, reverted)
 		}
-
 		return nil
 	})
 
