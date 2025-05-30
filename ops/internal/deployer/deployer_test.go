c28c34b157504385f52be9224c5e7aa921578a7e
geoknee <georgeknee@googlemail.com>
2025-05-30 18:23:45 +0100

    add TestBinaryInvocation

--- ops/internal/deployer/deployer_test.go
+++ ops/internal/deployer/deployer_test.go
@@ -91,3 +91,16 @@ func TestVersionsMapInitialization(t *testing.T) {
 	require.True(t, exists, "expected key 'op-contracts/v1.6.0' not found in contractVersions map")
 	require.Equal(t, actualVersion, expectedVersion)
 }
+
+func TestBinaryInvocation(t *testing.T) {
+	if CACHE_DIR == "" {
+		t.Skip(cacheDirEnvVar + " is not set")
+	}
+	lgr := log.NewLogger(log.NewTerminalHandlerWithLevel(os.Stderr, log.LevelInfo, false))
+	deployer, err := NewOpDeployer(lgr, "tag://op-contracts/v1.6.0", CACHE_DIR)
+	require.NoError(t, err)
+
+	output, err := deployer.runCommand("--help")
+	require.NoError(t, err)
+	require.NotNil(t, output)
+}
