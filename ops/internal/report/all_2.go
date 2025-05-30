627d9d93d07b2a1f81f2a0cedcf9c01716d019e4
Samuel Stokes <sam.adam.stokes@gmail.com>
2025-05-30 16:12:46 -0400

    report: add all_2.go

---
+++ ops/internal/report/all_2.go
@@ -0,0 +1,40 @@
+package report
+
+import (
+	"context"
+	"time"
+
+	"github.com/ethereum-optimism/superchain-registry/ops/internal/config"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+func ScanAll_2(
+	ctx context.Context,
+	l1RpcUrl string,
+	rpcClient *rpc.Client,
+	statePath string,
+	chainCfg *config.StagedChain,
+) Report {
+	var report Report
+	var err error
+	report.L1, err = ScanL1(
+		ctx,
+		rpcClient,
+		*chainCfg.DeploymentTxHash,
+		chainCfg.DeploymentL1ContractsVersion,
+	)
+	if err != nil {
+		report.L1Err = err
+	}
+	report.L2, err = ScanL2_2(
+		statePath,
+		chainCfg.ChainID,
+		l1RpcUrl,
+	)
+	if err != nil {
+		report.L2Err = err
+	}
+
+	report.GeneratedAt = time.Now()
+	return report
+}
