1f5f91bb863a614206965f6a28cb02c5e87c7fbf
Samuel Stokes <sam.adam.stokes@gmail.com>
2025-06-03 16:20:42 -0400

    deployer: support v0.4.0-rc.3

--- ops/internal/deployer/state.go
+++ ops/internal/deployer/state.go
@@ -34,6 +34,12 @@ var standardV3State []byte
 //go:embed configs/v3-intent.toml
 var standardV3Intent []byte
 
+//go:embed configs/v4-state.json
+var standardV4State []byte
+
+//go:embed configs/v4-intent.toml
+var standardV4Intent []byte
+
 func ReadOpaqueStateFile(p string) (OpaqueState, error) {
 	f, err := os.Open(p)
 	if err != nil {
@@ -73,6 +79,8 @@ func GetStateMerger(version string) (StateMerger, error) {
 		return MergeStateV2, nil
 	case 3:
 		return MergeStateV3, nil
+	case 4:
+		return MergeStateV4, nil
 	default:
 		return nil, fmt.Errorf("unsupported deployer version: %d", versionNum)
 	}
@@ -126,6 +134,88 @@ func MergeStateV3(userState OpaqueState) (OpaqueMap, OpaqueState, error) {
 	// V2 is correct here. V3's state is the same as V2, except with a
 	// slightly different intent that contains operator fee fields.
 	return mergeStateV2(userState, stdIntent, stdState)
+}
+
+func MergeStateV4(userState OpaqueState) (OpaqueMap, OpaqueState, error) {
+	l1ChainID, err := userState.ReadL1ChainID()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read L1 chain ID: %w", err)
+	}
+	stdIntent, err := StandardIntentV4(l1ChainID)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create standard intent: %w", err)
+	}
+	stdState, err := StandardStateV4(l1ChainID)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create standard state: %w", err)
+	}
+	return mergeStateV4(userState, stdIntent, stdState)
+}
+
+func mergeStateV4(userState OpaqueState, stdIntent OpaqueMap, stdState OpaqueState) (OpaqueMap, OpaqueState, error) {
+	userStateNode := dasel.New(userState)
+	stdIntentNode := dasel.New(stdIntent)
+	stdStateNode := dasel.New(stdState)
+
+	appliedIntentNode, err := userStateNode.Query("appliedIntent")
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read applied intent: %w", err)
+	}
+
+	// Helper function to aggregate copy errors
+	var copyErrs error
+	guard := func(err error) {
+		if err != nil {
+			copyErrs = multierror.Append(copyErrs, fmt.Errorf("failed to copy value: %w", err))
+		}
+	}
+
+	// First, set up the intent
+	guard(copyValue(appliedIntentNode, stdIntentNode, "l1ChainID"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].id"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].baseFeeVaultRecipient"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].l1FeeVaultRecipient"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].sequencerFeeVaultRecipient"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].roles.l1ProxyAdminOwner"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].roles.l2ProxyAdminOwner"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].roles.systemConfigOwner"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].roles.unsafeBlockSigner"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].roles.batcher"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].roles.proposer"))
+	guard(copyValue(appliedIntentNode, stdIntentNode, "chains.[0].roles.challenger"))
+
+	// Then, set up the state
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].id"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].OpChainProxyAdminImpl"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].AddressManagerImpl"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].L1Erc721BridgeProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].SystemConfigProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].OptimismMintableErc20FactoryProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].L1StandardBridgeProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].L1CrossDomainMessengerProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].OptimismPortalProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].DisputeGameFactoryProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].AnchorStateRegistryProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].FaultDisputeGameImpl"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].PermissionedDisputeGameImpl"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].DelayedWethPermissionedGameProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].DelayedWethPermissionlessGameProxy"))
+	guard(copyValue(userStateNode, stdStateNode, "opChainDeployments.[0].startBlock"))
+
+	if copyErrs != nil {
+		return nil, nil, copyErrs
+	}
+
+	intentResult, okIntent := stdIntentNode.InterfaceValue().(OpaqueMap)
+	if !okIntent {
+		return nil, nil, fmt.Errorf("internal error: synthesized intent is not OpaqueMapping, but %T", stdIntentNode.InterfaceValue())
+	}
+	stateResult, okState := stdStateNode.InterfaceValue().(OpaqueState)
+	if !okState {
+		return nil, nil, fmt.Errorf("internal error: synthesized state is not OpaqueMapping, but %T", stdStateNode.InterfaceValue())
+	}
+
+	return intentResult, stateResult, nil
 }
 
 func mergeStateV2(userState OpaqueState, stdIntent OpaqueMap, stdState OpaqueState) (OpaqueMap, OpaqueState, error) {
@@ -193,6 +283,10 @@ func mergeStateV2(userState OpaqueState, stdIntent OpaqueMap, stdState OpaqueSta
 	return intentResult, stateResult, nil
 }
 
+func StandardIntentV4(l1ChainID uint64) (OpaqueMap, error) {
+	return standardIntentV4(l1ChainID, standardV4Intent)
+}
+
 func StandardIntentV3(l1ChainID uint64) (OpaqueMap, error) {
 	return standardIntentV2(l1ChainID, standardV3Intent)
 }
@@ -203,6 +297,30 @@ func StandardIntentV2(l1ChainID uint64) (OpaqueMap, error) {
 
 func StandardIntentV1(l1ChainID uint64) (OpaqueMap, error) {
 	return standardIntentV1(l1ChainID, standardV1Intent)
+}
+
+func standardIntentV4(l1ChainID uint64, data []byte) (OpaqueMap, error) {
+	intent := make(OpaqueMap)
+	if err := toml.Unmarshal(data, &intent); err != nil {
+		panic(err)
+	}
+
+	var stdRoles validation.RolesConfig
+	switch l1ChainID {
+	case 1:
+		stdRoles = validation.StandardConfigRolesMainnet
+	case 11155111:
+		stdRoles = validation.StandardConfigRolesSepolia
+	default:
+		return nil, fmt.Errorf("unsupported L1 chain ID: %d", l1ChainID)
+	}
+
+	root := dasel.New(intent)
+	mustPutString(root, "superchainRoles.SuperchainProxyAdminOwner", stdRoles.L1ProxyAdminOwner)
+	mustPutString(root, "superchainRoles.ProtocolVersionsOwner", stdRoles.ProtocolVersionsOwner)
+	mustPutString(root, "superchainRoles.SuperchainGuardian", stdRoles.Guardian)
+
+	return intent, nil
 }
 
 func standardIntentV2(l1ChainID uint64, data []byte) (OpaqueMap, error) {
@@ -248,6 +366,10 @@ func standardIntentV1(l1ChainID uint64, data []byte) (OpaqueMap, error) {
 	mustPutString(root, "superchainRoles.protocolVersionsOwner", stringWrapper("0x79ADD5713B383DAa0a138d3C4780C7A1804a8090"))
 
 	return intent, nil
+}
+
+func StandardStateV4(l1ChainID uint64) (OpaqueState, error) {
+	return standardState(l1ChainID, validation.Semver400, standardV4State)
 }
 
 func StandardStateV3(l1ChainID uint64) (OpaqueState, error) {
